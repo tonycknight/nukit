960ca93753924286df5bd10f4a7fcf8096a4bd67
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-17 15:30:52 +0000

    WIP

---
+++ test/Nukit.Tests.Unit/TestUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Abstractions;
+using NSubstitute;
+
+namespace Nukit.Tests.Unit
+{
+    internal static class TestUtils
+    {
+        public static IFileSystem CreateFileSystem() => Substitute.For<IFileSystem>();
+
+        public static IFileSystem SetCurrentDirectory(this IFileSystem fs, string path)
+        {
+            fs.Directory.GetCurrentDirectory().Returns(path);
+
+            return fs;
+        }
+
+        public static IFileSystem SetDirectoryExists(this IFileSystem fs, string path, bool exists = true)
+        {
+            fs.DirectoryInfo.New(path).Exists.Returns(exists);
+            return fs;
+        }
+    }
+}
