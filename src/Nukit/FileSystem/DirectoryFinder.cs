e04d7fd714c24741df286485d1d6757010153d77
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-17 15:40:33 +0000

    DirectoryFinder abstraction over Matcher

---
+++ src/Nukit/FileSystem/DirectoryFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+
+namespace Nukit.FileSystem
+{
+    internal interface IDirectoryFinder
+    {
+        IEnumerable<string> GetDirectories(string path, string[]? includedPaths, string[]? excludedPaths);
+    }
+
+    [ExcludeFromCodeCoverage] // The Match class cannot accept injected IFileSystem instances
+    internal class DirectoryFinder : IDirectoryFinder
+    {
+        public IEnumerable<string> GetDirectories(string path, string[]? includedPaths, string[]? excludedPaths)
+        {
+            var matcher = Create(includedPaths, excludedPaths);
+
+            var dir = CreateDirectoryWrapper(path);
+
+            return matcher.Execute(dir).Files.Select(m => m.Path);
+        }
+
+        private Matcher Create(string[]? includedPaths, string[]? excludedPaths)
+        {
+            var result = new Matcher();
+            if(includedPaths != null)
+                result.AddIncludePatterns(includedPaths);
+            if(excludedPaths != null)
+                result.AddExcludePatterns(excludedPaths);
+            return result;
+        }
+
+        private static DirectoryInfoWrapper CreateDirectoryWrapper(string path) => new(new DirectoryInfo(path));
+    }
+}
